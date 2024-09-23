f409fcafb303eccd456fce83209eda511e34d6b4
Gabriel Eduardo Ríos Morales <24020030@uadeo.mx>
2024-09-22 23:50:10 -0700

    Add files via upload

---
+++ IOS NOMBRE main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    char nombre [20], apellido[20];
+
+    cout << "Introduzca su nombre: " << endl;
+    cin >> nombre;
+    cout << "Introduzca su apellido: " << endl;
+    cin >> apellido;
+    cout <<"hola " << nombre << " " << apellido << " buenos dias. " << endl;
+
+    return 0;
+}
